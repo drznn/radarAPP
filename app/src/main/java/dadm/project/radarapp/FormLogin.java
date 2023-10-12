9ff8618e6ac084dedbb4f16cefd61c1d4de036c6
André Albuquerque <a.albuquerque@live.com>
2023-10-12 19:00:31 -0300

    criacao tela de registro

--- app/src/main/java/dadm/project/radarapp/FormLogin.java
+++ app/src/main/java/dadm/project/radarapp/FormLogin.java
@@ -2,13 +2,31 @@ package dadm.project.radarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
 
+    private TextView text_tela_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+        iniciarComponentes();
+        text_tela_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormLogin.this, FormRegistro.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void iniciarComponentes(){
+        text_tela_registro = findViewById(R.id.text_registro);
+
+    }
+
 }
---
+++ app/src/main/java/dadm/project/radarapp/FormRegistro.java
@@ -0,0 +1,14 @@
+package dadm.project.radarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_registro);
+    }
+}
